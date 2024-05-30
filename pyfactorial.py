bcb2b8179f59ad6da0da9f43baae66f6de59b775
HelloGHR <gauri3235@gmail.com>
2024-05-30 11:50:03 +0530

    Intial Commit

--- pyfactorial.py
+++ pyfactorial.py
@@ -1,48 +1,55 @@
-# Python program to find the factorial of a number provided by the user.
+"""# Python program to find the factorial of a number provided by the user.
 
-class Fact:
-   #initialising the constructor
+# change the value for a different result
+num = 7
+
+# To take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+      factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+"""
+
+class Assignment1:
    def __init__(self):
-      self.num=None
-      self.factorial=None
-  # function for taking the valid input
-   def Input_from_user(self):
-      while True:
-         try:
-               self.num = int(input("Enter a number: "))
-               return self.num
-         except ValueError:
-               print("Invalid input. Please enter a valid integer.")
-   # function to find the factorial
-   def Find_factorial(self):
-      self.factorial = 1
-      # check if the number is negative, positive or zero
+      self.num=0
+      self.factorial=1
+      self.is_avail=True
+      self.stored=list()
+   def take_input(self,text=None):
+      self.num=int(input(text))
+      return self.num
+   def Factorial(self,num=None):
+      if num is not None:
+        self.num=num
       if self.num < 0:
-         print("Sorry, factorial does not exist for negative numbers")
+        self.is_avail=False
+        self.stored.append(self.is_avail)
       elif self.num == 0:
-         print("The factorial of 0 is 1")
+        self.Factorial=1
       else:
-         for i in range(1,self.num + 1):
-           self.factorial = self.factorial*i
-      return self.factorial
-   #function to print the output
-   def Print_output(self):
-      print("The factorial of",self.num,"is",self.factorial)
-
-#object creation
-
-obj = Fact()
-obj.Input_from_user()
-obj.Find_factorial()
-obj.Print_output()
+       for i in range(1,self.num + 1):
+        self.factorial = self.factorial*i
+        self.stored.append(self.factorial)
+   def display_result(self):
+      if self.is_avail==False:
+         print("Sorry, factorial does not exists for negative numbers")
+         exit()
+      else:
+         return self.factorial
+   def get_stored_data(self):
+     return self.stored
 
 
-
-
-
-
-
-   
 
 
 
